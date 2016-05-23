dd16c28de8eebc601ac32db0d290c96ba7fe4adf
Ruyu You <727809166@qq.com>
2016-05-23 17:31:13 +0800

    finish separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,26 @@
-'use strict';
-
 function thousands_separators(num) {
-  
+    var numArray = num.toString().split('.');
+    var thousandSepartorNum = buildThousandSepartor(numArray);
+    return thousandSepartorNum;
+}
+
+
+function buildThousandSepartor(numArray) {
+    var integerArray = numArray[0].toString().split('');
+    var integer = '';
+    for (var i = 0; i < integerArray.length; i++) {
+        integer += integerArray[i];
+        if (Math.abs(integerArray.length - 1 - i) % 3 === 0 && integerArray.length - 1 - i !== 0) {
+            integer += ',';
+        }
+    }
+
+    if (numArray[1]) {
+        return integer + '.' + numArray[1];
+    } else {
+        return integer;
+    }
+
 }
 
 module.exports = thousands_separators;
